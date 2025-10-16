c690de99e9907cd577bf23446db1015694d9be4b
David Roman Rodriguez <dromrod529@iescarrillo.es>
2025-10-16 14:57:38 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import http = require("http");
+
 //Tipos de Variables
 var x ="David";
 if(true){
@@ -38,7 +40,7 @@ if(typeof(desconocido)=="string"){
 
 
 // ARRAYS
-let numeros:number[]=[1,2,3,4];3
+let numeros:number[]=[1,2,3,4];
 console.log(numeros[1])
 
 let mixto:(number|string)[]=["David","Roman",22,2003];
@@ -52,4 +54,124 @@ let numeros2:number[]=[5,6,7,8];
 let numeros3= {...numeros,...numeros2};
 console.log(numeros3);
 numeros3[0]=33;
+    //Metodos para manejar arrays
+        let add:number[]=[];
+        add.push(1,2,3,4,5675);//insertar al final
+        console.log(add);
+        add.pop();//retirar el ultimo numero annadido
+        console.log(add)
+
+        add.unshift(0); //inserta al principio del array
+        console.log(add);        
+        add.shift()       //retira el primer dato del array
+        console.log(add);
+
+        console.log(add.length);//longitud del array
+
+        //Operadores
+            //valor!.length ->La exclamacion indica que el valor no es null
+            //valor?.length -> El interrogante indica que en caso de undefined o null no se ejecuta la funcion length
+
+        //Buscar elementos
+        let frutas:String[]=["manzana","pera","fresa","kiwi"];
+        console.log(frutas.indexOf("kiwi"));//Devuelve el indice del valor buscado
+            //INCLUDES
+            console.log(frutas.includes("pera"));//Devuelve true/false dependiendo de si lo encuentra en al array
+       
+            //FIND
+            console.log(frutas.find(( word:String)=>{return word.length>=5}));//Devuelve el primer valor que cumpla una condicion
+            
+            //FIND INDEX
+            console.log(frutas.findIndex(( word:String)=>{return word.length>=5}));//Devuelve el indice del  primer valor que cumpla una condicion
+
+            frutas.forEach((valor)=>{valor.length>=5?console.log(valor):null}); //Realiza toda la logica que le indiques en la funcion flecha
+
+            let modfruta=frutas.map((valor)=>{return valor.toUpperCase()}); // Realiza todos los cambios indicados en la funcion flecha
+            console.log(modfruta);
+
+            let filterfrutas= frutas.filter((valor)=>{return frutas.length>=5});
+            console.log(filterfrutas);
+
+            let enteros=[10,20,30,40];
+        enteros.reduce((acu,eActu)=>{return acu+=eActu});
+
+        let concat= frutas.reduce((ante,actu)=>{return ante+=" "+actu})
+        console.log(concat)
+
+        console.log(frutas.sort());// Ordena el array por string 
+        console.log(enteros.sort((a,b)=>{return b-a}))// Con una funcion flecha explicando el orden de ordenacion podemos solucionarlo
+
+
+        console.log(frutas.slice(0,2));
+
+        console.log(frutas.join("."))
+
+        //Tuplas
+
+            let info:[String,number];
+            info=["David",18];
+            console.log(info)
+        
+        //Diccionarios
+            let diccionario ={1:"Paco",2:"Kike"};
+            console.log(diccionario);
+
+        //FUNCIONES DECLARADAS
+        /**
+         * Suma dos numeros enteros
+         * @param a Primero numero entero a sumar
+         * @param b Segundo numero entero a sumar
+         * @returns Devuelve la suma de ambos 
+         */
+        function sumar(a:number,b:number):number{
+            
+            return a+b;
+        }
+        let suma = sumar(10,20)
+        console.log(suma)
+
+        //FUNCIONES ANONIMAS
+
+        const resta=function(a:number,b:number,c?:number):number{return a-b} //No tiene hosting por lo que solo se puede llamar en el codigo despues de ser declaradas. 
+        console.log(resta(10,5))                                             //La ? sirve para poner un parametro opcional
+            //PARAMENTROS POR DEFECTO
+            function potencia(x:number,y:number=2){
+                return x**y
+            }
+            console.log(potencia(2))
+            console.log(potencia(2,6))
+        //FUNCIONES FLECHA (parametros)=>{operaciones}
+        const fflecha=(a:number)=>{return a};
+
+        //FUNCION CALLBACK
+        function math(a:number,b:number,operacion:(a:number,b:number)=>number):number{
+
+            return operacion(a,b);
+        }
+        const fsuma=(a:number,b:number)=>{return a+b;};
+
+        math(1,3,fsuma)
+        
+        //funcion con parametros infinitos
+            function numbers(...valores:number[]){
+                    let suma= valores.reduce((pre:number,sig:number)=>{return pre+=sig})
+                    console.log(suma);
+            }
+            numbers(1,2,3,4)
+        
+        //FUNCIONES ASINCRONAS
+            interface DataApi{
+                message:string
+                status:string
+            }
+
+            async function getApiData(url:string):Promise<DataApi> {
+                const resp=await fetch(url)
+                const datos= resp.json() as Promise<DataApi>;
+                return datos;
+            }
+           getApiData("https://dog.ceo/api/breeds/image/random")
+           .then((value:DataApi)=>{console.log(value.message)}) //Si encuentra los resustados correctamente usamos el then para realizar lo que queramos
+           .catch((error)=>{console.log(error)});   //Si da un error con el catch controlamos lo que ocurrira en este caso mostrar el error
+           
 
